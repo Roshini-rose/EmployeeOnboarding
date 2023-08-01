7ed34e3a7e54c28f5ca18b0b9065023e481a32d8
mubeen <ahmedmubeen.ma243@gmail.com>
2023-08-01 12:32:21 +0530

    m

--- EmployeeOnboarding/Migrations/20230731112411_Onboarding.Designer.cs
+++ EmployeeOnboarding/Migrations/20230731112411_Onboarding.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EmployeeOnboarding.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EmployeeOnboarding.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230731112411_Onboarding")]
+    partial class Onboarding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
