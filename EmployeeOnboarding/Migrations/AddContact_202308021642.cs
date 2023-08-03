f775dfe8b938d051096d94efece7a7482c9aa739
Pooja <Pooja@LAPTOP-SVGA662F>
2023-08-03 16:39:54 +0530

    p

--- EmployeeOnboarding/Migrations/AddContact_202308021642.cs
+++ EmployeeOnboarding/Migrations/AddContact_202308021642.cs
@@ -32,16 +32,3 @@ namespace EmployeeOnboarding.Migrations
         }
     }
 }
- public int Id { get; set; }
-[ForeignKey("EmpGen_Id")]
-public int EmpGen_Id { get; set; }
-public string Personal_Emailid { get; set; }
-public double Contact_no { get; set; }
-public string? Emgy_Contactperson { get; set; }
-public int? Emgy_Contactrelation { get; set; }
-public double? Emgy_Contactno { get; set; }
-public DateTime Date_Created { get; set; }
-public DateTime? Date_Modified { get; set; }
-public string Created_by { get; set; }
-public string? Modified_by { get; set; }
-public string Status { get; set; }
