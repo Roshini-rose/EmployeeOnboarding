2517d79c556ee82e0c4526ac2b7d117a4568f327
Boscojohnson Arulraj <bosco.johnson@ideassion.com>
2023-08-04 16:09:20 +0530

    Updated

--- EmployeeOnboarding/Contracts/IAdminRepository.cs
+++ EmployeeOnboarding/Contracts/IAdminRepository.cs
@@ -6,13 +6,11 @@ namespace EmployeeOnboarding.Contracts
 {
     public interface IAdminRepository
     {
-      Task <List<Dashboard1VM>> GetPendingEmployeeDetails();
-        Task<List<Dashboard1VM>> GetInvitedEmployeeDetails();
-        Task<List<DashboardVM>> GetEmployeeDetails();
-
-      Task DeleteEmployee(string[] employeeId);   
-        
-        Task <List<PersonalInfoVM>>? GetPersonalInfo(int id);
-       
+       Task <List<Dashboard1VM>> GetPendingEmployeeDetails();
+       Task<List<Dashboard1VM>> GetInvitedEmployeeDetails();
+       Task<List<Dashboard1VM>> GetRejectedEmployeeDetails();
+       Task<List<DashboardVM>> GetEmployeeDetails();
+       Task DeleteEmployee(string[] employeeId);    
+       Task <List<PersonalInfoVM>>? GetPersonalInfo(int id);
     }
 }
