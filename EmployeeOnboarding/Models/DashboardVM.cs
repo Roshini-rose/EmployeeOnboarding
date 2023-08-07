34b21f3660c9aefff4047c6045ee4223829498e2
Boscojohnson Arulraj <bosco.johnson@ideassion.com>
2023-08-07 16:44:16 +0530

    Updated

--- EmployeeOnboarding/Models/DashboardVM.cs
+++ EmployeeOnboarding/Models/DashboardVM.cs
@@ -4,9 +4,10 @@ namespace EmployeeOnboarding.Models
 {
     public class DashboardVM
     {
+        public int EmpGen_Id { get; set; }
         public string Empid { get; set; }
         public string Empname { get; set; }
-        public string designation { get; set; }
+       // public string designation { get; set; }
         public double Contact { get; set; }
         public string Email { get; set; }
         public string education { get; set; }
