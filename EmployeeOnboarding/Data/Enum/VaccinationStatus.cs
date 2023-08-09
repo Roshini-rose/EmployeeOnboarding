34736e10cb826d8a1e7e1a1377e2b1316f1f3b8a
Boscojohnson Arulraj <bosco.johnson@ideassion.com>
2023-08-09 13:14:08 +0530

    Updated

---
+++ EmployeeOnboarding/Data/Enum/VaccinationStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Tracing;
+
+namespace EmployeeOnboarding.Data.Enum
+{
+    public enum VaccinationStatus
+    {
+        [Display(Name ="None")]
+        None=1,
+        [Display(Name ="Partially Vaccinated")]
+        Partially_Vaccinated=2,
+        [Display(Name ="Fully Vaccinated")]
+        Fully_Vaccinated=3
+    }
+}
