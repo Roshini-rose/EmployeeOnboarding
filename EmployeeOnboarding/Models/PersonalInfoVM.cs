0505e327a119c9e004d2bdda57583503f481e16e
mubeen <ahmedmubeen.ma243@gmail.com>
2023-08-10 12:30:02 +0530

    frontEnd

--- EmployeeOnboarding/Models/PersonalInfoVM.cs
+++ EmployeeOnboarding/Models/PersonalInfoVM.cs
@@ -29,15 +29,5 @@ namespace EmployeeOnboarding.Models
         public List<EducationDetailsVM> educationDetailsVMs { get; set; }
         public List<ExperienceVM> experienceVMs { get; set; }
     }
-    public static class EnumExtensionMethods
-    {
-        public static string GetEnumDescription(this Enum enumValue)
-        {
-            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
-
-            var descriptionAttributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-            return descriptionAttributes.Length > 0 ? descriptionAttributes[0].Description : enumValue.ToString();
-        }
-    }
+    
 }
